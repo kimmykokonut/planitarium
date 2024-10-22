8ebc9d02d3e7c80664adaf69c2f1e9eea74223df
Kim Robinson <kimmykokonut@gmail.com>
2024-10-22 09:17:51 -0700

    add viewsets and update urls, endpoints created

--- planitarium/urls.py
+++ planitarium/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, TagViewSet, TaskViewSet
+
+router = DefaultRouter()
+# register vsets w router, endpoints auto generated
+router.register(r'catgories', CategoryViewSet)
+router.register(r'tags', TagViewSet)
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', include(router.urls)),
 ]
--- planitarium/views.py
+++ planitarium/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Category, Tag, Task
+from .serializers import CategorySerializer, TagSerializer, TaskSerializer
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 def index(request):
   return HttpResponse("You are at the index page!")
+
+class CategoryViewSet(viewsets.ModelViewSet):
+  #ModelViewSet dRF has Crud under the hood. specify model/serializer
+  queryset = Category.objects.all()
+  serializer_class = CategorySerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+  queryset = Tag.objects.all()
+  serializer_class = TagSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+  queryset = Task.objects.all()
+  serializer_class = TaskSerializer
